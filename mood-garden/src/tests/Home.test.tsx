28fda18c114eee19be246fb1d4640ce2c5a95695
qisun724 <qisun724@gmail.com>
2024-11-11 17:35:28 -0800

    Reformatted layout for readability

--- mood-garden/src/tests/Home.test.tsx
+++ mood-garden/src/tests/Home.test.tsx
@@ -2,10 +2,12 @@ import { render, screen } from "@testing-library/react";
 import { Home } from "../pages/Home";
 
 test("renders Homepage", () => {
-  render(<Home/>);
+  render(<Home />);
   const homepageMessage = screen.getByText(/Plant the Seeds of Better Sleep/i);
   expect(homepageMessage).toBeInTheDocument();
 
-  const anotherMessage = screen.getByText(/Track your sleep, tend to your garden, and watch your healthy habits bloom./i);
+  const anotherMessage = screen.getByText(
+    /Track your sleep, tend to your garden, and watch your healthy habits bloom./i
+  );
   expect(anotherMessage).toBeInTheDocument();
 });
