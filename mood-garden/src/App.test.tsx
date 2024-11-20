d5aab558155a99a11dbf8ff19639caf90a1092c0
dania <146884362+03txt@users.noreply.github.com>
2024-11-20 15:48:47 -0800

    google oauth test

--- mood-garden/src/App.test.tsx
+++ mood-garden/src/App.test.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 
 test('renders learn react link', () => {
-  render(<BrowserRouter><App /></BrowserRouter>);
+  render(<BrowserRouter>   <GoogleOAuthProvider clientId="130824139626-b0tcvptr6rr7ka9l8c0ipmvfik3fc2e5.apps.googleusercontent.com"> <App /> </GoogleOAuthProvider> </BrowserRouter>);
   const homepageMessage = screen.getByText(/Plant the Seeds of Better Sleep/i);
   expect(homepageMessage).toBeInTheDocument();
 });
