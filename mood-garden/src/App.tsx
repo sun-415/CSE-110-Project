b1bbc8fb7010cb541ad1e9af9568c5cfed225e33
dania <146884362+03txt@users.noreply.github.com>
2024-11-20 15:30:24 -0800

    google oauth

--- mood-garden/src/App.tsx
+++ mood-garden/src/App.tsx
@@ -6,46 +6,23 @@ import { Route, Routes } from "react-router-dom";
 import { Navbar } from "./components/navbar";
 import "./App.css";
 import Login from "./components/Login";
+import { PointsProvider } from "./context/PointsContext";
 
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <Navbar/>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home/>} />
         <Route path="/checkin" element={<CheckIn />} />
         <Route path="/progress" element={<Progress />} />
       </Routes>
+      <PointsProvider/>
       <Login/>
     </div>
   );
 }
 
-export default App;
-
-import React from "react";
-import { Home } from "./pages/Home";
-import { CheckIn } from "./pages/CheckIn";
-import { Progress } from "./pages/Progress";
-import { Route, Routes } from "react-router-dom";
-import { Navbar } from "./components/navbar";
-import "./App.css";
-import { PointsProvider } from "./context/PointsContext";
-
-function App() {
-  return (
-    <div>
-      <PointsProvider>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/checkin" element={<CheckIn />} />
-          <Route path="/progress" element={<Progress />} />
-        </Routes>
-      </PointsProvider>
-    </div>
-  );
-}
 
 export default App;
