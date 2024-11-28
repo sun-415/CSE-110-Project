f2b243e04c8707ecb415124da412d04387569615
eholguin1 <eholguin@ucsd.edu>
2024-11-28 01:45:22 -0800

    Update App.tsx

--- mood-garden/src/App.tsx
+++ mood-garden/src/App.tsx
@@ -7,19 +7,22 @@ import { Navbar } from "./components/navbar";
 import "./App.css";
 //import Login from "./components/Login";
 import { PointsProvider } from "./context/PointsContext";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 
 function App() {
   return (
     <div>
-      <PointsProvider>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/checkin" element={<CheckIn />} />
-          <Route path="/progress" element={<Progress />} />
-        </Routes>
-      </PointsProvider>
+      <GoogleOAuthProvider clientId="130824139626-b0tcvptr6rr7ka9l8c0ipmvfik3fc2e5.apps.googleusercontent.com">
+        <PointsProvider>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/checkin" element={<CheckIn />} />
+            <Route path="/progress" element={<Progress />} />
+          </Routes>
+        </PointsProvider>
+      </GoogleOAuthProvider>
     </div>
   );
 }
