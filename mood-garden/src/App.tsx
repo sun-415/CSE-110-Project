f33f10386e00e3dce63c1f5660fc95b9aae03d88
eholguin1 <eholguin@ucsd.edu>
2024-11-28 01:37:15 -0800

    moved login button

--- mood-garden/src/App.tsx
+++ mood-garden/src/App.tsx
@@ -5,7 +5,7 @@ import { Progress } from "./pages/Progress";
 import { Route, Routes } from "react-router-dom";
 import { Navbar } from "./components/navbar";
 import "./App.css";
-import Login from "./components/Login";
+//import Login from "./components/Login";
 import { PointsProvider } from "./context/PointsContext";
 
 
@@ -19,7 +19,6 @@ function App() {
           <Route path="/checkin" element={<CheckIn />} />
           <Route path="/progress" element={<Progress />} />
         </Routes>
-        <Login />
       </PointsProvider>
     </div>
   );
