8dc933533971c7f99af055153a6921da3832c148
dania <146884362+03txt@users.noreply.github.com>
2024-11-30 15:33:02 -0800

    user profile set up

--- mood-garden/src/App.tsx
+++ mood-garden/src/App.tsx
@@ -1,6 +1,7 @@
 import { Home } from "./pages/Home";
 import { CheckIn } from "./pages/CheckIn";
 import { Progress } from "./pages/Progress";
+import { Profile } from "./pages/Profile";
 import { Route, Routes } from "react-router-dom";
 import { Navbar } from "./components/navbar";
 import "./App.css";
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/progress" element={<Progress />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </AuthProvider>
     </div>
